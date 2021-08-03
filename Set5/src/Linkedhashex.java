cb3c1b3edc62f8f24df383566b530bbd3dd44cc7
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-03 18:23:49 +0530

    12th commit
    Upload all files

---
+++ Set5/src/Linkedhashex.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Linkedhashex {
+
+	public static void main(String[] args) {
+		 LinkedHashSet<String> lhset = new LinkedHashSet<String>();
+         lhset.add("Z");
+         lhset.add("PQ");
+         lhset.add("N");
+         lhset.add("O");
+         lhset.add("KK");
+         lhset.add("FGH");
+         System.out.println(lhset);
+          LinkedHashSet<Integer> lhset2 = new LinkedHashSet<Integer>(); 
+         lhset2.add(99);
+         lhset2.add(7);
+         lhset2.add(0);
+         lhset2.add(67);
+         lhset2.add(89);
+         lhset2.add(66);
+         System.out.println(lhset2);
+
+	}
+
+}
